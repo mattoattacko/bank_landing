80d40e5b5e78df34bc32d33294fc54d2fa5fd514
unknown <matt.mcquain@gmail.com>
2022-08-12 19:08:58 -0700

    further component refinements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import styles from './style'
+
+import { Billing, Business, CardDeal, Navbar, Hero, Stats, Testimonials, Clients, CTA, Footer } from './components'
 
 const App = () => (
+
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello World</h1>
+  {/* Navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+  {/* Hero: Main Splash */}
+  <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Hero />
+    </div>
+  </div>  
+  
+  <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Stats />
+      <Business />
+      <Billing />
+      <CardDeal />
+      <Testimonials />
+      <Clients />
+      <CTA />
+      <Footer />
+    </div>
   </div>
-)
+
+  </div>
+);
 
 
 export default App
