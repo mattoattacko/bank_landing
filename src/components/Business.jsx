82bfc786cf73416dbee8b0b129692572f24bd510
unknown <matt.mcquain@gmail.com>
2022-08-15 18:30:28 -0700

    Business component more fleshed out. Starting work on the Billing component.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,27 @@ import styles, { layout } from '../style'
 import { features } from '../constants'
 import Button from './Button'
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] feature-card ${index !== features.length - 1 ? 'mb-6' : 'mb-0'}`}>
+    <div className={`w-[64px] h-[64px] rounded-full bg-dimBlue ${styles.flexCenter}`}>
+      <img 
+        src={icon} 
+        alt='icon' 
+        className='w-[50%] h-[50%] object-contain'
+      />
+    </div>
+
+    <div className='flex flex-col flex-1 ml-3'>
+      <h4 className='text-white font-poppins font-semibold text-[18px] leading-[23px] mb-1'>
+        {title}
+      </h4>
+      <p className='text-dimWhite font-poppins font-normal text-[16px] leading-[24px] mb-1'>
+        {content}
+      </p>
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
     <section id='features' className={layout.section}>
@@ -16,6 +37,12 @@ const Business = () => {
         </p>
 
         <Button styles='mt-10' />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
 
     </section>
