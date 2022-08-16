582b46a411d5d94f94aa14ea4ee3c83a4a4744ef
unknown <matt.mcquain@gmail.com>
2022-08-15 18:58:17 -0700

    working on CardDeal

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className='sm:block hidden' /> in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Financial freedom is a right for everyone. But how do you find the right card? Who knows! We can try help you though.
+      </p>
+
+      <Button styles='mt-10'/>
+    </div>
+  </section>
+)
+
 
 export default CardDeal
