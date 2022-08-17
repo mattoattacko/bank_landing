993a597dfcef6eda55a864807d1f0b3959105beb
unknown <matt.mcquain@gmail.com>
2022-08-16 22:08:34 -0700

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,9 +29,12 @@ const Footer = () => (
             <h4 className='font-poppins font-medium text-[18px] leading-[18px] text-white'>
               {footerLink.title}
             </h4>
-            <ul>
+            <ul className='list-none mt-4'>
               {footerLink.links.map((link,index) => (
-                <li key={link.name}>
+                <li 
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}  `}  
+                >
                   {link.name}
                 </li>
               ))}
