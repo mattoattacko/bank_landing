4957e88b6a439ba3e4db30046e451909ab9c7162
unknown <matt.mcquain@gmail.com>
2022-08-16 21:21:09 -0700

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col `}>
+    <div className={`${styles.boxWidth}`}>
+
+    </div>
+  </section>
+)
+
 
 export default Footer
