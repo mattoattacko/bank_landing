e4fe62effb76a7ff187ff6bfb93eedb94f865348
unknown <matt.mcquain@gmail.com>
2022-08-16 22:34:03 -0700

    footer mostly complete. Final touchups needed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,13 +33,33 @@ const Footer = () => (
               {footerLink.links.map((link,index) => (
                 <li 
                   key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}  `}  
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}  `}
+                  // 'index !==' stuff above is to add a property to the last link in the list. if you don't do this, the last link will have a bottom margin of 0, and we don't want that.
                 >
                   {link.name}
                 </li>
               ))}
             </ul>
           </div>
+        ))}
+      </div>
+    </div>
+
+    {/* social media container */}
+    <div className='flex w-full justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
+      <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white '>
+      Copyright © 2022 Hoobank. All Rights Reserved.
+      </p>
+
+      {/* icons */}
+      <div className='flex flex-row md:mt-0 mt-6 '>
+        {socialMedia.map((social, index) => (
+          <img 
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer hover:opacity-50 ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+          />
         ))}
       </div>
     </div>
